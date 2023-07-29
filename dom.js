3c45d91a6b7487b7b91055a6f7e2c119b3adbf8d
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2023-07-29 19:45:12 +0530

    add object with li tag

--- dom.js
+++ dom.js
@@ -21,13 +21,35 @@
 //header.style.borderBottom = "solid 3px #000";
 
 //
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+//let items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
 //items[1].textContent = 'hello';
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
+//items[0].style.fontWeight = 'bold';
+//items[1].style.fontWeight = 'bold';
+//items[2].style.fontWeight = 'bold';
+//items[3].style.fontWeight = 'bold';
 
-items[2].style.backgroundColor = 'green';
+/*for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}*/
+
+//items[2].style.backgroundColor = 'green';
+
+// GETELEMENTTAGLINE
+
+//let li = document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].style.backgroundColor = 'skyblue';
+
+//for(let i=0;i<li.length;i++)
+//{
+//  li[i].style.backgroundColor = 'skyblue';
+//}
+
+
+
+
+
