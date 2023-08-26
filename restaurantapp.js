25c3d1fbc6eb177089e522408368194268932081
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2023-08-26 12:00:50 +0530

    used asynchoronous function

--- restaurantapp.js
+++ restaurantapp.js
@@ -10,24 +10,32 @@ function orderFood(event){
         recipe,
         seat
     }
+    
+    postOnCrudCrud();
 
-    axios.post("https://crudcrud.com/api/ef4cfc06cf77446db309cddb21cc7617/bookingOrder",myList)
-        .then((response) => {
+
+    async function postOnCrudCrud () {
+        try{
+         const response = await axios.post("https://crudcrud.com/api/e112ec0d0dab41669f73b35d893f5b35/bookingOrder",myList)
             showListOnScreen(response.data);
-        })
-        .catch((error) => {
+        }
+        catch(error) {
             console.log(error);
-        })
+        }
     }
+}
 
-    window.addEventListener("DOMContentLoaded" , () => {
-        axios.get("https://crudcrud.com/api/ef4cfc06cf77446db309cddb21cc7617/bookingOrder")
-            .then((response) => {
-                for(let i=0;i<response.data.length;i++)
-                {
-                    showListOnScreen(response.data[i]);
-                }
-            })
+    window.addEventListener("DOMContentLoaded" , async() => {
+        try {
+        const response = await axios.get("https://crudcrud.com/api/e112ec0d0dab41669f73b35d893f5b35/bookingOrder")
+            response.data.forEach((element) => {
+                showListOnScreen(element);
+            });
+        }
+        catch(err) {
+            console.log(err);
+        }
+        
     })
 
     function showListOnScreen(userList){
@@ -35,21 +43,20 @@ function orderFood(event){
         const parentElement = document.getElementById(`${userList.seat}`);
 
         const childElement = `<li id=${userList._id}><i> '${userList.cost}'-'${userList.recipe}'-'${userList.seat}' <i>
-                                <button class="deletebutton btn-primary" onclick=deleteUser('${userList.seat}','${userList._id}')>Delete Order</button></li>`
+                                <button class="deletebutton btn-primary btn-sm" onclick=deleteUser('${userList.seat}','${userList._id}')>Delete Order</button></li>`
 
         parentElement.innerHTML += childElement;
     }
 
-    function deleteUser(tablenumber,userid){
-
-        axios.delete(`https://crudcrud.com/api/ef4cfc06cf77446db309cddb21cc7617/bookingOrder/${userid}`)
-            .then((response) => {
-                removeOnDisplay(tablenumber,userid);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+    async function deleteUser(tablenumber,userid){
+        try {
+        const response = await axios.delete(`https://crudcrud.com/api/e112ec0d0dab41669f73b35d893f5b35/bookingOrder/${userid}`)
+            removeOnDisplay(tablenumber,userid);
         }
+         catch(err) {
+        console.log(err);
+    }
+    }
     function removeOnDisplay(tablenumber,userid){
 
         const parentEle = document.getElementById(tablenumber);
